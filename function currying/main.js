0e9d35692404b7702f29ce6f11a5f8d989959d00
Sourabh Bharamkar <bharamkarsourabh8989@gmail.com>
2023-01-04 01:53:02 +0530

    function currying using bind and closures

---
+++ function currying/main.js
@@ -0,0 +1,28 @@
+// function currying 
+
+// let multiply=function(x,y){
+//     console.log(x*y)
+// }
+
+// let multiplyByTwo=multiply.bind(this,2)
+// multiplyByTwo(5)
+
+// let multiplyByThree=multiply.bind(this,3)
+// multiplyByThree(5)
+
+//function currying using closures 
+
+let multiply=function(x){
+    return function(y){
+        console.log(x*y)
+    }
+}
+
+let multiplyByTwo=multiply(2)
+multiplyByTwo(3)
+
+let multiplyByThree=multiply(3)
+multiplyByThree(10)
+
+
+
